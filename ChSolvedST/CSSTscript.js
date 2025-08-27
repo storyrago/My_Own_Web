2e9ea0391f466e79c79c8941b3ef5bcc6260e139
storyrago <cscsh2068@naver.com>
2025-08-27 17:31:35 +0900

    Fix sty;e ChSolvedST

--- ChSolvedST/CSSTscript.js
+++ ChSolvedST/CSSTscript.js
@@ -37,7 +37,7 @@ async function getStats(handle) {
     
     $result.innerHTML = `
       <div class="stat">닉네임: ${data.handle}</div>
-      <div class="stat">티어: <img src="https://static.solved.ac/tier_small/${data.tier}.svg" alt="티어"> (${data.tier})</div>
+      <div class="stat" id ="tierImg"><img src="https://static.solved.ac/tier_small/${data.tier}.svg" alt="티어"></div>
       <div class="stat">랭크: ${data.rank}</div>
       <div class="stat">최대 스트릭: ${data.maxStreak}</div>
       <div class="stat">해결 문제 수: ${data.solvedCount}</div>
